01e4b27177f0f4a1ef4ed425806783c604646273
HellCatVN <hellcatvn@gmail.com>
2020-11-19 10:13:41 +0700

    New bigplayer theme

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,15 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-import { renderToString } from 'react-dom/server';
-import { Player } from '../app/components/Minimal/Player';
-import { getNowPlayingData } from '../app/spotify';
-import axios from 'axios';
-import ejs from 'ejs';
-import fs from 'fs';
+import { NowRequest, NowResponse } from "@vercel/node";
+import { renderToString } from "react-dom/server";
+import { Player } from "../app/components/Minimal/Player";
+import { getNowPlayingData } from "../app/spotify";
+import axios from "axios";
+import ejs from "ejs";
+import fs from "fs";
 
 export default async function (req: NowRequest, res: NowResponse) {
   let { theme } = req.query;
   if (!theme) {
-    theme = '0';
+    theme = "0";
   }
   const {
     item = {},
@@ -17,23 +17,23 @@ export default async function (req: NowRequest, res: NowResponse) {
     progress_ms: progress = 0,
   } = await getNowPlayingData();
   let { duration_ms: duration, name: track } = item;
-  const artist = (item.artists || []).map(({ name }) => name).join(', ');
+  const artist = (item.artists || []).map(({ name }) => name).join(", ");
   const { images = [] } = item.album || {};
   let text;
-  if (theme == '0') {
+  if (theme == "0") {
     if (track == null) {
-      track = 'No song is playing';
+      track = "No song is playing";
     }
     const coverURL = images[0]?.url || null;
     if (coverURL) {
       var cover = await getBase64Image(coverURL);
     }
     let file = fs.readFileSync(
-      __dirname + '/../app/components/AudioVirtualize/index.ejs',
-      'utf-8'
+      __dirname + "/../app/components/AudioVirtualize/index.ejs",
+      "utf-8"
     );
     text = ejs.render(file, { title: track, artist: artist, cover: cover });
-  } else if (theme == '1') {
+  } else if (theme == "1") {
     // Get Smallest Thumbnail
     const coverURL = images[images.length - 1]?.url || null;
     if (coverURL) {
@@ -42,17 +42,27 @@ export default async function (req: NowRequest, res: NowResponse) {
     text = renderToString(
       Player({ cover, track, artist, progress, duration, isPlaying })
     );
+  } else if (theme == "2") {
+    const coverURL = images[0]?.url || null;
+    if (coverURL) {
+      var cover = await getBase64Image(coverURL);
+    }
+    let file = fs.readFileSync(
+      __dirname + "/../app/components/BigPlayer/index.ejs",
+      "utf-8"
+    );
+    text = ejs.render(file, { title: track, artist: artist, cover: cover });
   }
-  res.setHeader('Content-Type', 'image/svg+xml');
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
   return res.status(200).send(text);
 }
 
 async function getBase64Image(coverURL) {
   const bufferImage = await axios.get(coverURL, {
-    responseType: 'arraybuffer',
+    responseType: "arraybuffer",
   });
   return `data:image/jpeg;base64,${Buffer.from(bufferImage.data).toString(
-    'base64'
+    "base64"
   )}`;
 }
